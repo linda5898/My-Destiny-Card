a25932883bf8c60a790259335c0e8bd10f251fd0
linda5898 <linda5898@outlook.com>
2024-08-31 16:54:48 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders MyDestinyCard title', () => {
+test('renders mydestinycard title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/MyDestinyCard/i);
+  const linkElement = screen.getByText(/mydestinycard/i);
   expect(linkElement).toBeInTheDocument();
 });
